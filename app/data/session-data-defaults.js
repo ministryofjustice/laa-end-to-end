78bd4d8d0c36e07a9b53abbfa26d3cc0e8f76fd2
KristinaIankova <kristina.iankova@justice.gov.uk>
2025-02-19 11:05:35 +0000

    Add caseworker flows

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-  // Insert values here
+  'financiallyEligible':'No',
 
 }
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,16 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//Financial eligibility routes
+
+
+router.post('/financial_eligibility_answer', function(request, response) {
+
+    var financiallyEligibile = request.session.data['financiallyEligible']
+    if (financiallyEligibile  == "yes"){
+        response.redirect('/caseworker_means/financial_eligibility_')
+    } else {
+        response.redirect('/caseworker_means/financial_eligibility')
+    }
+})
